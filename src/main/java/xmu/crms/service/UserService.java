4e63c42c6a64d0da70468c7690522816cad30829
moonlight <1241621063@qq.com>
2017-12-11 20:25:42 +0800

    Update UserService

--- src/main/java/xmu/crms/service/UserService.java
+++ src/main/java/xmu/crms/service/UserService.java
@@ -13,8 +13,18 @@ import xmu.crms.bo.UserBO;
  *
  */
 public class UserService {
-private List listPresentStudent(BigInteger seminarId,BigInteger classId) {
-List<UserBO> list=new ArrayList<UserBO>();
-return list;
-}
+	/**
+	 * 查询班级出勤学生信息.
+	 * ＜p＞根据讨论课id及班级id得到讨论课所在的班级出勤学生信息(方法的详细说明第一行)＜br＞ * (方法的详细说明第二行)
+	 * @author XXX
+	 * @param seminarId 讨论课的id
+	 * @param classId 班级的id
+	 * @return List 处于出勤状态的学生列表
+	 * @see (具体的Service的方法，写法参考文档。例如XXXService#toString() )
+	 */
+	private List listPresentStudent(BigInteger seminarId, BigInteger classId) {
+		List<UserBO> list = new ArrayList<UserBO>();
+		return list;
+	}
+	
 }
