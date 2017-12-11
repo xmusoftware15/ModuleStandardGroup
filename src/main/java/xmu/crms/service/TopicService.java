c330b98b6637965685eb3502481d16aefb497d2d
moonlight <1241621063@qq.com>
2017-12-11 20:23:21 +0800

    Restart 重启

---
+++ src/main/java/xmu/crms/service/TopicService.java
@@ -0,0 +1,10 @@
+package xmu.crms.service;
+/**
+ * 
+ * @author ModuleStandardGroup
+ * @version 1.00
+ *
+ */
+public class TopicService {
+
+}
---
+++ src/main/java/xmu/crms/service/UserService.java
@@ -0,0 +1,20 @@
+package xmu.crms.service;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import xmu.crms.bo.UserBO;
+
+/**
+ * 
+ * @author ModuleStandardGroup
+ * @version 1.00
+ *
+ */
+public class UserService {
+private List listPresentStudent(BigInteger seminarId,BigInteger classId) {
+List<UserBO> list=new ArrayList<UserBO>();
+return list;
+}
+}
