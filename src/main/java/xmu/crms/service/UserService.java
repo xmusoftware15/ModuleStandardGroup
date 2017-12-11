7ed4fd772cad02dcec513c052e7484ad1318be49
lingyun <342637442@qq.com>
2017-12-12 01:37:17 +0800

    useservice

--- src/main/java/xmu/crms/service/UserService.java
+++ src/main/java/xmu/crms/service/UserService.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import xmu.crms.bo.UserBO;
-import xmu.crms.service.SchoolService;
 
 /**
  * @author qinlingyun
@@ -17,17 +16,37 @@ public class UserService {
 	 * <p>获取讨论课所在的班级的出勤学生名单<br>
 	 * @param seminarId 讨论课ID
 	 * @param classId 班级ID
-	 * @return List<User> 处于出勤状态的学生列表
+	 * @return List<User> 处于出勤状态的学生的列表
 	 */
 	private List listPresentStudent(BigInteger seminarId, BigInteger classId) {
 		
-		List<UserBO> list = new ArrayList<UserBO>();
+		List<UserBO> list = new ArrayList<UserBO>();		
 		
-		return List<User>;
+		return List<UserBO>;
 		
 	};
 
 
+	/**
+	 * 方法简述.
+	 * <p>用户解绑(.NET)<br>.NET解绑是置空手机号Phone,J2EE解绑是置空微信号unionId<br>
+	 * @param UserId 用户id
+	 * @return true 解绑成功 false 解绑失败
+	 */
+	private boolean deleteUser(BigInteger UserId) {
+		
+		if(true) {
+			
+			return true;
+		
+		} else{
+			
+			return false;
+			
+		}
+	};	
+
+	
 	/**
 	 * 方法简述.
 	 * <p>获取讨论课所在班级缺勤学生名单<br>
@@ -36,12 +55,10 @@ public class UserService {
 	 * @return List<User> 处于缺勤状态的学生列表
 	 */
 	private List listAbsenceStudent(BigInteger seminarId,BigInteger classId) {
-		
+
 		List<UserBO> list = new ArrayList<UserBO>();
 		
-		UserDAO.listAbsenceStudent(BigInteger seminarId, BigInteger classId);
-		
-		return List<User>;
+		return List<UserBO>;
 		
 	};
 
@@ -51,16 +68,11 @@ public class UserService {
 	 * <p>根据用户Id获取用户的信息<br> 
 	 * @param UserId 用户Id
 	 * @return UserBO 用户信息
-	 * @see 调用的Service方法：SchoolService.getSchoolBySchoolId(BigInteger SchoolId);
 	 */
 	private UserBo getUserByUserId(BigInteger UserId) {
 		
-		List<UserBO> list = new ArrayList<UserBO>();
-		
-		SchoolService.getSchoolBySchoolId(BigInteger SchoolId);
-		
-		UserDAO.getUserByUserId(BigInteger UserId);
-		
+		/**SchoolService.getSchoolBySchoolId(BigInteger SchoolId);*/
+
 		return UserBO;
 		
 	};
@@ -75,9 +87,7 @@ public class UserService {
 	 */
 	private boolean updateUserByUserId(BigInteger UserId, UserBO user) {
 		
-		UserDao.updateUserById(BigInteger UserId, UserDO user);
-		
-		if() {
+		if(true) {
 			
 			return true;
 		
@@ -99,37 +109,35 @@ public class UserService {
 	 * @param success_url 微信OAuth2授权后跳转到的网址
 	 * @return user 该用户信息
 	 */
-	private User signInWeChat(BigInteger UserId,String code,String state,String success_url) {
+	private UserBO signInWeChat(BigInteger UserId,String code,String state,String success_url) {
 		
-		return user;
+		return userBO;
 		
 	};	
 	
 	
 	/**
 	 * 方法简述.
-	 * <p>手机号登录 (.Net使用)<br> 
+	 * <p>手机号登录 (.Net使用)<br>User中只有Phone和Password，用于判断用户名密码是否正确<br> 
 	 * @param user 用户信息(手机号Phone和密码Password)
 	 * @return user 该用户信息
-	 * @see User中只有Phone和Password，用于判断用户名密码是否正确
 	 */
 	private UserBO signInPhone(UserBO user) {
 		
-		return user;
+		return UserBO;
 		
 	};		
 
 	
 	/**
 	 * 方法简述.
-	 * <p>手机号注册 (.Net使用)<br> 
+	 * <p>手机号注册 (.Net使用)<br>User中只有Phone和Password，UserId是注册后才有并且在数据库自增<br> 
 	 * @param user 用户信息(手机号Phone和密码Password)
 	 * @return user 该用户信息
-	 * @see User中只有Phone和Password，UserId是注册后才有并且在数据库自增
 	 */
 	private UserBO signUpPhone(UserBO user) {
 		
-		return user;
+		return UserBO;
 		
 	};
 	
@@ -144,7 +152,9 @@ public class UserService {
 	 */	
 	private List listUserByClassId(BigInteger classId,String numBeginWith,String nameBeginWith) {
 
-		return List<User>;
+		List<UserBO> list = new ArrayList<UserBO>(); 
+		
+		return List<UserBO>;
 		
 	};
 	
@@ -158,6 +168,6 @@ public class UserService {
 	private BigInteger getUserIdByUserName(String UserName) {
 		
 		return UserId;
-	}
+	};
 		
 }
