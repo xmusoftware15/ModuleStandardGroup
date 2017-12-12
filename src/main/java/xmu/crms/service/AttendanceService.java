532e51a019b857ccb1a0d15e1f1c709b28f55932
AiqiLiu <LiuAiqi@liuaiqideMacBook-Pro.local>
2017-12-12 10:14:25 +0800

    AttendanceService-2

--- src/main/java/xmu/crms/service/AttendanceService.java
+++ src/main/java/xmu/crms/service/AttendanceService.java
@@ -13,6 +13,7 @@ public class AttendanceService {
 	 * @param classId 班级的id
 	 * @param userId 学生的id
 	 * @return true（添加签到信息成功）/false（添加签到信息未成功）
+     * @see ClassService#getClassByClassId(BigInteger classId)
 	 */
 	boolean insertAttendanceById(Biginteger classId,Biginteger userId)
 	{
