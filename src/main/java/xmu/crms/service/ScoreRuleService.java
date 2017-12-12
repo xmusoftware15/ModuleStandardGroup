d0c5827f83d89b418ba876f0776cbcae3cc8074d
junzz <34027020+zhouzj151921@users.noreply.github.com>
2017-12-13 01:09:34 +0800

    zhouzhongjun

--- src/main/java/xmu/crms/service/ScoreRuleService.java
+++ src/main/java/xmu/crms/service/ScoreRuleService.java
@@ -16,14 +16,13 @@ public class ScoreRuleService {
 
 
 		/**
-		 * 方法简述.
-		 * <p>按classId和CourseId删除ClassSelection<br>  
+		 * 按classId和CourseId删除ScoreRule
 		 * @author zhouzhongjun
 		 * @param BigInteger courseId 班级Id
 	     * @param BigInteger classId 班级Id
 	     * @return true/false 是否成功删除
 		 */
-		boolean deleteClassSelectionByClassId(BigInteger courseId,BigInteger classId) {
+		boolean deleteScoreRuleById(BigInteger courseId,BigInteger classId) {
 			
 			//删除自己
 			return true;
