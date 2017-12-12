e8bbb4d66417bd33d580f1512780dd3eeefbd888
AiqiLiu <LiuAiqi@liuaiqideMacBook-Pro.local>
2017-12-12 09:43:12 +0800

    Attendance/SchoolService

--- src/main/java/xmu/crms/service/AttendanceService.java
+++ src/main/java/xmu/crms/service/AttendanceService.java
@@ -6,5 +6,20 @@ package xmu.crms.service;
  *
  */
 public class AttendanceService {
+	/**
+	 * 添加学生签到信息
+	 * ＜p＞根据班级id，学生id进行签到，在该操作内部调用相应API获取地理位置进行判断
+	 * @author LiuAiqi
+	 * @param classId 班级的id
+	 * @param userId 学生的id
+	 * @return true（添加签到信息成功）/false（添加签到信息未成功）
+	 * @see AttendanceService#insertAttendanceById(BigInteger, BigInteger)
+	 */
+	boolean insertAttendanceById(Biginteger classId,Biginteger userId)
+	{
+		if(true)
+			return true;
+		else return false;
+	}
 
 }
--- src/main/java/xmu/crms/service/SchoolService.java
+++ src/main/java/xmu/crms/service/SchoolService.java
@@ -6,5 +6,60 @@ package xmu.crms.service;
  *
  */
 public class SchoolService {
-
+	/**
+	 * 按城市名称查学校
+	 * ＜p＞根据城市名称，查询出在该城市的所有学校
+	 * @author LiuAiqi
+	 * @param city 城市名称
+	 * @return List<SchoolBO> 学校列表
+	 * @see SchoolService#listSchoolByCity(String city)
+	 */
+	List<SchoolBO> listSchoolByCity(String city)
+	{
+		List<SchoolBO> list = new ArrayList<SchoolBO>();		
+		return List<SchoolBO>;
+	}
+	
+	/**
+	 * 添加学校
+	 * ＜p＞根据学校详细的信息，添加相应学校
+	 * @author LiuAiqi
+	 * @param school 学校的信息
+	 * @return true（添加学校成功）/false（添加学校未成功）
+	 * @see SchoolService#insertSchool(School school)
+	 */
+	boolean insertSchool(School school)
+	{
+		if(true)
+			return true;
+		else 
+			return false;
+	}
+	
+	/**
+	 * 获取省份列表
+	 * ＜p＞获取所有省份的名称
+	 * @author LiuAiqi
+	 * @return List<String> 省份名称列表
+	 * @see SchoolService#listProvince()
+	 */
+	List<String> listProvince()
+	{
+		List<String> list = new ArrayList<String>();		
+		return List<String> list;
+	}
+	
+	/**
+	 * 获取城市列表
+	 * ＜p＞根据省份名称，获取该省份所有城市名称，返回城市名称列表
+	 * @author LiuAiqi
+	 * @param province 省份名称
+	 * @return List<String> 城市名称列表
+	 * @see SchoolService#listCity(String province)
+	 */
+	List<String> listCity(String province)
+	{
+		List<String> list = new ArrayList<String>();		
+		return List<String> list;
+	}
 }
