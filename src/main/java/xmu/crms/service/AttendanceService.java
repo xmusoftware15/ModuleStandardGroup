700bab641f201442bc8cb98ae7134faa012c70bc
AiqiLiu <LiuAiqi@liuaiqideMacBook-Pro.local>
2017-12-12 21:27:32 +0800

    Attendance/SchoolService-修改2

--- src/main/java/xmu/crms/service/AttendanceService.java
+++ src/main/java/xmu/crms/service/AttendanceService.java
@@ -11,15 +11,18 @@ import java.math.BigInteger;
 public class AttendanceService {
 	/**
 	 * 添加学生签到信息
-	 * ＜p＞根据班级id，学生id进行签到，在该操作内部调用相应API获取地理位置进行判断
+	 * <p>根据班级id，讨论课id，学生id，经度，纬度进行签到<br>*在方法中通过班级id，讨论课id获取当堂课发起签到的位置
 	 * @author LiuAiqi
 	 * @param classId 班级的id
+	 * @param seminarId 讨论课的id
 	 * @param userId 学生的id
+	 * @param longitude 经度
+	 * @param latitude 纬度
 	 * @return true（添加签到信息成功）/false（添加签到信息未成功）
-     * @see ClassService#getClassByClassId(BigInteger classId)
 	 */
-	boolean insertAttendanceById(BigInteger classId,BigInteger userId)
+	boolean insertAttendanceById(BigInteger classId, BigInteger seminarId, BigInteger userId， double longitude, double latitude)
 	{
+		
 		if(true) 
 		{
 			return true;
