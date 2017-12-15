ef93ca74c21f18430c8932d7428d18bb9eaab8a0
GrafZeppelinZwei <34131904+GrafZeppelinZwei@users.noreply.github.com>
2017-12-15 20:16:01 +0800

    Modify UserService
    Modify UserServices to interface, add method listCourserByTeacher, and split login methods to new interface LoginService

--- src/main/java/xmu/crms/service/UserService.java
+++ src/main/java/xmu/crms/service/UserService.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import xmu.crms.bo.AttendanceBO;
+import xmu.crms.bo.CourseBO;
 import xmu.crms.bo.UserBO;
 
 /**
- * @author ModuleStandardGroup
- * @version 1.00
+ * @author ModuleStandardGroup/YeHongjie
+ * @version 1.10
  */
-public class UserService {
+public interface UserService {
+	
 	/**
 	 * 添加学生签到信息.
 	 * <p>根据班级id，讨论课id，学生id，经度，纬度进行签到<br>*在方法中通过班级id，讨论课id获取当堂课发起签到的位置
@@ -23,33 +25,17 @@ public class UserService {
 	 * @param latitude 纬度
 	 * @return true（添加签到信息成功）/false（添加签到信息未成功）
 	 */
-
-	boolean insertAttendanceById(BigInteger classId, BigInteger seminarId, BigInteger userId, double longitude, double latitude)
-	{
-		
-		if(true) 
-		{
-			return true;
-		}
-		else 
-		{
-			return false;
-		}
-	}
+	public boolean insertAttendanceById(BigInteger classId, BigInteger seminarId, BigInteger userId, double longitude, double latitude);
 	
 	/**
 	 * 获取学生签到信息.
 	 * <p>根据班级id，讨论课id获取当堂课签到信息
 	 * @author LiuAiqi
 	 * @param classId 班级的id
+	 * @param seminarId 讨论课id
 	 * @return list 当堂课签到信息
 	 */
-	private List<AttendanceBO> listAttendanceById(BigInteger classId)
-	{
-		
-		List<AttendanceBO> list = new ArrayList<AttendanceBO>();		
-		return list;
-	}
+	public List<AttendanceBO> listAttendanceById(BigInteger classId, BigInteger seminarId);
 
 	/**
 	 * 手机号注册.
@@ -58,44 +44,7 @@ public class UserService {
 	 * @param user 用户信息(手机号Phone和密码Password)
 	 * @return user 该用户信息
 	 */
-	private UserBO signUpPhone(UserBO user) {
-		
-		return user;
-		
-	}
-	
-
-	/**
-	 * 微信登录.
-	 * <p>微信登录<br> 
-	 * @author qinlingyun
-	 * @param userId 用户Id
-	 * @param code 微信小程序/OAuth2授权的Code
-	 * @param state 微信OAuth2授权的state。对于小程序，值恒为 MiniProgram
-	 * @param successUrl 微信OAuth2授权后跳转到的网址
-	 * @return user 该用户信息
-	 */
-	private UserBO signInWeChat(BigInteger userId,String code,String state,String successUrl) {
-
-		UserBO user = new UserBO();		
-		
-		return user;
-		
-	}
-	
-	
-	/**
-	 * 手机号登录.
-	 * <p>手机号登录 (.Net使用)<br>*User中只有phone和password，用于判断用户名密码是否正确<br>
-	 * @author qinlingyun 
-	 * @param user 用户信息(手机号Phone和密码Password)
-	 * @return user 该用户信息
-	 */
-	private UserBO signInPhone(UserBO user) {
-		
-		return user;
-		
-	}
+	public UserBO signUpPhone(UserBO user);
 	
 	
 	/**
@@ -107,12 +56,7 @@ public class UserService {
 	 * @see CourseService#listCourseByUserId(BigInteger userId)
 	 * @see CourseService#deleteCourseByCourseId(BigInteger courseId)
 	 */
-	private boolean deleteTeacherAccount(BigInteger userId) {
-		
-		boolean isDeleted = true;
-		
-		return isDeleted;
-	}	
+	public boolean deleteTeacherAccount(BigInteger userId);
 	
 	
 	/**
@@ -123,12 +67,7 @@ public class UserService {
 	 * @return true 解绑成功 false 解绑失败
 	 * @see ClassService#deleteCourseSelectionById(BigInteger userId,BigInteger classId)
 	 */		
-	private boolean deleteStudentAccount(BigInteger userId) {
-	
-		boolean isDeleted = true;
-		
-		return isDeleted;
-	}
+	public boolean deleteStudentAccount(BigInteger userId);
 	
 	
 	/**
@@ -139,13 +78,7 @@ public class UserService {
 	 * @return user 用户信息
 	 * @see SchoolService#getSchoolBySchoolId(BigInteger schoolId)
 	 */
-	private UserBO getUserByUserId(BigInteger userId) {
-
-		UserBO user = new UserBO();
-		
-		return user;
-		
-	}
+	public UserBO getUserByUserId(BigInteger userId);
 
 	/**
 	 * 根据用户名获取用户ID.
@@ -154,10 +87,7 @@ public class UserService {
 	 * @param userName 用户名
 	 * @return userId 用户ID
 	 */
-	private BigInteger getUserIdByUserName(String userName) {
-		BigInteger userId=new BigInteger("100");
-		return userId;
-	}
+	public List<BigInteger> listUserIdByUserName(String userName);
 	
 	/**
 	 * 根据用户ID修改用户信息.
@@ -165,14 +95,9 @@ public class UserService {
 	 * @author qinlingyun
 	 * @param userId 用户Id
 	 * @param user 用户信息
-	 * @return true 修改成功 false 修改失败
+	 * @return list 用户id列表
 	 */
-	private boolean updateUserByUserId(BigInteger userId, UserBO user) {
-		
-		boolean isDeleted = true;
-		
-		return isDeleted;
-	}
+	public boolean updateUserByUserId(BigInteger userId, UserBO user);
 	
 
 	/**
@@ -184,13 +109,7 @@ public class UserService {
 	 * @param nameBeginWith 姓名开头
 	 * @return list 用户列表
 	 */	
-	private List listUserByClassId(BigInteger classId,String numBeginWith,String nameBeginWith) {
-
-		List<UserBO> list = new ArrayList<UserBO>(); 
-		
-		return list;
-		
-	}
+	public List listUserByClassId(BigInteger classId,String numBeginWith,String nameBeginWith);
 	
 
 	/**
@@ -200,13 +119,7 @@ public class UserService {
 	 * @param userName 用户名
 	 * @return list 用户列表
 	 */	
-	private List listUserByUserName(String userName) {
-
-		List<UserBO> list = new ArrayList<UserBO>(); 
-		
-		return list;
-		
-	}
+	public List<UserBO> listUserByUserName(String userName);
 	
 	
 	/**
@@ -216,14 +129,10 @@ public class UserService {
 	 * @param seminarId 讨论课ID
 	 * @param classId 班级ID
 	 * @return list 处于出勤状态的学生的列表
+	 * @see UserService #listAttendanceById(BigInteger, BigInteger)
+	 * @see UserService #getUserByUserId(BigInteger)
 	 */
-	private List listPresentStudent(BigInteger seminarId, BigInteger classId) {
-		
-		List<UserBO> list = new ArrayList<UserBO>();		
-		
-		return list;
-		
-	}
+	public List<UserBO> listPresentStudent(BigInteger seminarId, BigInteger classId);
 
 
 	/**
@@ -232,13 +141,19 @@ public class UserService {
 	 * @param seminarId 讨论课ID
 	 * @param classId 班级ID
 	 * @return list 处于缺勤状态的学生列表
+	 * @see UserService #listUserByClassId(BigInteger, String, String)
+	 * @see UserService #listPresentStudent(BigInteger, BigInteger)
 	 */
-	private List listAbsenceStudent(BigInteger seminarId,BigInteger classId) {
-
-		List<UserBO> list = new ArrayList<UserBO>();
-		
-		return list;
-		
-	}
+	public List<UserBO> listAbsenceStudent(BigInteger seminarId,BigInteger classId);
 	
+	/**
+	 * 根据教师名称列出课程名称.
+	 * <p>根据教师名称列出课程名称<br>
+	 * @author yexiaona
+	 * @param teacherName 教师名称
+	 * @return list 课程列表
+	 * @see UserService #listUserByUserName(String userName)
+	 * @see CourseService #listCourseByUserId(BigInteger userId)
+	 */
+	public List<CourseBO> listCourseByTeacherName(String teacherName);
 }
