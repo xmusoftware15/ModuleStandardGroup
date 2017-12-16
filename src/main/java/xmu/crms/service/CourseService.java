8ad339f647b593142ee62f45159dc1ace53bfc39
CaoXingmei <34434873+CaoXingmei@users.noreply.github.com>
2017-12-16 18:12:09 +0800

    courseService
    将courseService的class变成了interface，将private变成了public，
    将函数体删除了

--- src/main/java/xmu/crms/service/CourseService.java
+++ src/main/java/xmu/crms/service/CourseService.java
@@ -13,107 +13,82 @@ import xmu.crms.bo.ClassBO;
  * @version 1.00
  *
  */
-public class CourseService {
+public interface CourseService {
 	/**
 	 * 按userId获取与当前用户相关联的课程列表.
 	 * <p>老师与他相关联的课程列表<br> 
-	 * @author zhouzhongjun
+	 * @author ZhouZhongjun
 	 * @param userId 用户Id
 	 * @return null 课程列表
 	 */
-	private List<CourseBO> listCourseByUserId(BigInteger userId) {
-		return null;
-	};	
+	public List<CourseBO> listCourseByUserId(BigInteger userId);
+	
 	
 	/**
 	 * 按userId创建课程.
-	 * @author zhouzhongjun
+	 * @author ZhouZhongjun
 	 * @param userId 用户Id
 	 * @param course 课程信息
 	 * @return courseId 新建课程的id
 	 */
-	private  BigInteger insertCourseByUserId(BigInteger userId,CourseBO course) {
-		
-		BigInteger courseId = null;
-		return  courseId;
-	};	
+	public  BigInteger insertCourseByUserId(BigInteger userId,CourseBO course);
 	
 	
 	/**
 	 * 按courseId获取课程 .
-	 * @author zhouzhongjun
+	 * @author ZhouZhongjun
 	 * @param courseId 课程Id
 	 * @return course
 	 */
-	private  CourseBO getCourseByCourseId(BigInteger courseId) {
-		
-		CourseBO course=new CourseBO();
-		return course;
-		
-	};	
+	public  CourseBO getCourseByCourseId(BigInteger courseId);
 	
 	
 	/**
 	 * 传入courseId和course信息修改course信息.
-	 * @author zhouzhongjun
+	 * @author ZhouZhongjun
      * @param courseId 课程Id
 	 * @param course 课程信息
 	 * @return true修改成功  false修改失败
 	 */
-	private boolean updateCourseByCourseId(BigInteger courseId,CourseBO course) {
-		
-		return true;
-		
-	};	
+	public boolean updateCourseByCourseId(BigInteger courseId,CourseBO course);
+	
 	
 	/**
 	 * 按courseId删除课程.
 	 * <p>先根据courseID删除Seminar 和 class,然后再将course的信息删除<br>  
-	 * @author zhouzhongjun
+	 * @author ZhouZhongjun
      * @param courseId 课程Id
-	 *@see SeminarService #deleteSemiarByCourseId(BigInteger courseId)
-	 *@see ClassService   #deleteClassByCourseId(BigInteger courseId)
-	 *@return true删除成功  false删除失败
+	 * @see SeminarService #deleteSemiarByCourseId(BigInteger courseId)
+	 * @see ClassService   #deleteClassByCourseId(BigInteger courseId)
+	 * @return true删除成功  false删除失败
 	 */
-	boolean deleteCourseByCourseId(BigInteger courseId) {
-		
-		
-		//删除自己
-		return true;
-	};
+	public boolean deleteCourseByCourseId(BigInteger courseId);
 	
 
 	
 	/**
 	 * 根据课程名称获取课程列表.
 	 * <p>根据课程名称获取课程列表<br>
-	 * @author yexiaona
+	 * @author YeXiaona
 	 * @param courseName 课程名称
 	 * @return list 课程列表
-
 	 * @see CourseService #listCourseByCourseName(String courseName)
 	 * @see CourseService #getCourseByCourseId(BigInteger courseId)
 	 */
-	private List<CourseBO> listCourseByCourseName(String courseName)
-	{
-		List<CourseBO> list=new ArrayList<CourseBO>();
-		return list;
-	}
+	public List<CourseBO> listCourseByCourseName(String courseName);
+	
 	
 	/**
 	 * 按课程名称获取班级列表.
 	 * <p>根据课程名获取课程ID，通过课程ID获取班级列表<br>
-	 * @author yexiaona
+	 * @author YeXiaona
 	 * @param courseName 课程名称
 	 * @return list 班级列表
 	 * @see CourseService #listCourseByCourseName(String courseName)
 	 * @see ClassService #listClassByCourseId(BigInteger courseId)
 	 */
-	private List<ClassBO> listClassByCourseName(String courseName)
-	{
-		List<ClassBO> list=new ArrayList<ClassBO>();
-		return list;
-	}
+	public List<ClassBO> listClassByCourseName(String courseName);
+	
 	
 	/**
 	 * 按教师名称获取班级列表.
@@ -124,23 +99,16 @@ public class CourseService {
 	 * @see CourseService #listCourseByTeacherName(String teacherName)
 	 * @see ClassService #listClassByCourseId(BigInteger courseId)
 	 */
-	private List<ClassBO> listClassByTeacherName(String teacherName)
-	{
-		List<ClassBO> list=new ArrayList<ClassBO>();
-		return list;
-	}
+	public List<ClassBO> listClassByTeacherName(String teacherName);
+	
 	
 	/**
-	 * 根据教师ID获取班级列表.
+	 * 根据教师ID获取班级列表.  
 	 * @author yexiaona
 	 * @param userId 教师ID
 	 * @return list 班级列表
 	 * @see CourseService #listCourseByUserId(BigInteger userId)
 	 */
-	private List<ClassBO> listClassByUserId(BigInteger userId)
-	{
-		List<ClassBO> list=new ArrayList<ClassBO>();
-		return list;
-	}
+	public List<ClassBO> listClassByUserId(BigInteger userId);
 }
 
