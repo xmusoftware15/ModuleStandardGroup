4e21a031ede7da5988c2fc24f0db752117205b76
moonlight <1241621063@qq.com>
2017-12-21 23:33:01 +0800

    dotnet同步

--- dotnet/CourseManagementSystem/CourseManagementSystem/Service/IGradeService.cs
+++ dotnet/CourseManagementSystem/CourseManagementSystem/Service/IGradeService.cs
@@ -1,123 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Numerics;
-using Xmu.Crms.Shared.Models;
+﻿using System.Collections.Generic;
 
 namespace Xmu.Crms.Shared.Service
 {
-    /**
- * 
- * @author Huhui QinLingyun ZhouZhongjun
- * @version 2.00
- *
- */
-    public interface GradeService
+    /// <summary>
+    /// @author Huhui QinLingyun ZhouZhongjun
+    /// @version 2.00
+    /// </summary>
+    public interface IGradeService
     {
+        /// <summary>
+        /// 按topicId删除学生打分表.
+        /// @author zhouzhongjun
+        /// </summary>
+        /// <param name="topicId">话题Id</param>
+        void DeleteStudentScoreGroupByTopicId(long topicId);
 
+        /// <summary>
+        /// 获取某学生所有讨论课的成绩.
+        /// @author qinlingyun
+        /// </summary>
+        /// <param name="userId">学生id</param>
+        /// <param name="seminarGroupId">讨论课小组id</param>
+        /// <returns>list 讨论课分数列表</returns>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.ISeminarGroupService.ListSeminarGroupIdByStudentId(System.Int64)"/>
+        List<int> ListSeminarGradeBySeminarGroupId(long userId, long seminarGroupId);
 
-        /**
-     * 按seminarGroupTopicId删除学生打分表.
-     *
-     * @param seminarGroupTopicId  小组话题表的Id
-     * @return true删除成功  false删除失败
-     * @throws IllegalArgumentException topicId格式错误时抛出
-     * @author zhouzhongjun
-     */
-        void DeleteStudentScoreGroupByTopicId(long seminarGroupTopicId);
+        /// <summary>
+        /// 提交对其他小组的打分.
+        /// @author Huhui
+        /// </summary>
+        /// <param name="userId">用户id</param>
+        /// <param name="seminarId">讨论课Id</param>
+        /// <param name="groupId">小组Id</param>
+        /// <param name="grade">分数</param>
+        void InsertGroupGradeByUserId(long userId, long seminarId, long groupId, long grade);
 
-        /**
-         * 获取某学生一堂讨论课信息.
-         * <p>获取某学生一堂讨论课的详细信息（包括成绩）<br>
-         *
-         * @param userId         学生id
-         * @param seminarGroupId 讨论课小组id
-         * @return seminarGroup 讨论课小组信息（包括成绩）
-         * @throws xmu.crms.exception.GroupNotFoundException 无此小组
-         * @throws IllegalArgumentException                  userId或seminarGrouopId格式错误
-         * @author qinlingyun
-         */
-        SeminarGroup GetSeminarGroupBySeminarGroupId(long userId, long seminarGroupId);
+        /// <summary>
+        /// 按ID设置小组报告分.
+        /// @author Huhui
+        /// </summary>
+        /// <param name="seminarGroupId">讨论课组id</param>
+        /// <param name="grade">分数</param>
+        void UpdateGroupByGroupId(long seminarGroupId, long grade);
 
-        /**
-         * 获取某学生所有讨论课的所有成绩
-         * <p>获取某学生所有讨论课的详细信息（包括成绩）<br>
-         *
-         * @param userId
-         * @return list 学生历史讨论课小组列表（包含成绩）
-         * @throws IllegalArgumentException userId格式错误
-         * @author qinlingyun
-         * @see SeminarGroupService#listSeminarGroupIdByStudentId(long)
-         * @see GradeService#getSeminarGroupBySeminarGroupId(long, long)
-         */
-        List<SeminarGroup> ListSeminarGradeByUserId(long userId);
-
-        /**
-         * 按课程id获取学生该课程所有讨论课
-         * <p>通过课程id获取该课程下学生所有讨论课详细信息（包括成绩）<br>
-         *
-         * @param userId
-         * @param courseId
-         * @return list 该课程下所有讨论课列表
-         * @throws IllegalArgumentException courseId格式错误
-         * @see SeminarService#listSeminarByCourseId(long)
-         * @see SeminarGroupService#listSeminarGroupBySeminarId(long)
-         * @see GradeService#listSeminarGradeByUserId(long) 
-         */
-        List<SeminarGroup> listSeminarGradeByCourseId(long userId, long courseId) ;
-
-        /**
-         * 提交对其他小组的打分.
-         *
-         * @param userId    用户id
-         * @param seminarId 讨论课Id
-         * @param groupId   小组Id
-         * @param grade     分数
-         * @param topicId   话题id
-         * @throws IllegalArgumentException topicId或userId或seminarId或groupId或grade格式错误
-         * @author Huhui
-         */
-        void insertGroupGradeByUserId(long topicId, long userId, long seminarId, long groupId, int grade);
-
-        /**
-         * 按ID设置小组报告分.
-         *
-         * @param seminar_group_id 讨论课组id
-         * @param grade            分数
-         * @throws GroupNotFoundException   未找到小组
-         * @throws IllegalArgumentException seminar_group_id或grade格式错误
-         * @author Huhui
-         */
-        void updateGroupByGroupId(long seminar_group_id, int grade);
-
-        /**
-         * 定时器方法.
-         * 讨论课结束后计算展示得分.
-         * <p>条件: 讨论课已结束<br>*GradeService<br>
-         *
-         * @param seminarId      讨论课ID
-         * @param seminarGroupId 小组ID
-         * @throws IllegalArgumentException seminarId或seminarGroupId格式错误
-         * @author qinlingyun
-         */
-        void countPresentationGrade(long seminarId, long seminarGroupId);
-
-
-        /**
-         * 定时器方法.
-         * 讨论课结束后计算本次讨论课得分.
-         * <p>条件: 讨论课已结束，展示得分已算出<br>*GradeService<br>
-         *
-         * @param seminarId      讨论课ID
-         * @param seminarGroupId 小组ID
-         * @throws IllegalArgumentException seminarId或seminarGroupId格式错误
-         * @author qinlingyun
-         */
-        void countGroupGradeBySerminarId(long seminarId, long seminarGroupId);
-
-
-
-
+        /// <summary>
+        /// 获取某学生的讨论课成绩列表.
+        /// @author qinlingyun
+        /// </summary>
+        /// <param name="userId">用户id</param>
+        /// <seealso cref="M:Xmu.Crms.Shared.Service.ISeminarGroupService.ListSeminarGroupBySeminarId(System.Int64)"/>
+        List<int> ListSeminarGradeByStudentId(long userId);
     }
 }
--- dotnet/CourseManagementSystem/CourseManagementSystem/Service/ITimerService.cs
+++ dotnet/CourseManagementSystem/CourseManagementSystem/Service/ITimerService.cs
@@ -1,39 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Numerics;
-using Xmu.Crms.Shared.Models;
-
-namespace Xmu.Crms.Shared.Service
+﻿namespace Xmu.Crms.Shared.Service
 {
-    /**
- * 定时器
- * 
- * @author qinlingyun liuaiqi
- * @version 2.00
- */
+    /// <summary>
+    /// 定时器
+    /// @author qinlingyun liuaiqi
+    /// @version 2.00
+    /// </summary>
     public interface ITimerService
     {
-        /**
-         * 此部分待根据数据库部分entity进行修改
-         * 向Event表插入数据.
-         * @author qinlingyun
-         * @param time 事件的时间
-         * @param beanName 方法名称
-         * @param paramMap 方法参数
-         */
-        //void InsertEvent(Date time, Bean beanName, HashMap<Integer, String> paramMap);
+        /// <summary>
+        /// 讨论课结束后计算展示得分.
+        /// @author qinlingyun
+        /// </summary>
+        /// 
+        /// 条件：讨论课结束
+        void CountPresentationGrade();
 
-        /**
-         * 每十分钟检查一次Event实体的状况
-         * @author qinlingyun
-         */
-        void Scheduled();
-
-
-
-
-
+        /// <summary>
+        /// 课前将固定小组复制一份作为讨论课小组名单.
+        /// @author qinlingyun
+        /// </summary>
+        /// 
+        /// 条件：上课前
+        void FixedGroupToSeminarGroup();
     }
 }
